83954daefc99a568dab601feffd1d474c228bf4c
life-exe <lifeexe.art@gmail.com>
2023-01-04 13:39:01 +0200

    basic project setup

---
+++ Source/FPS/FPSGameMode.cpp
@@ -0,0 +1,14 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "FPSGameMode.h"
+#include "FPSCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+AFPSGameMode::AFPSGameMode()
+	: Super()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPerson/Blueprints/BP_FirstPersonCharacter"));
+	DefaultPawnClass = PlayerPawnClassFinder.Class;
+
+}
---
+++ Source/FPS/FPSGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FPSGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AFPSGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AFPSGameMode();
+};
+
+
+
